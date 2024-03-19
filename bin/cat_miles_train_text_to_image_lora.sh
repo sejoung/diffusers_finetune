b7e3034987cb7c4116d9c56acb589e91ec1ae9e8
sejoung kim <sejoung@gmail.com>
2024-03-19 16:35:50 +0900

    로라 수정

--- bin/cat_miles_train_text_to_image_lora.sh
+++ bin/cat_miles_train_text_to_image_lora.sh
@@ -13,16 +13,19 @@ export OUTPUT_DIR="../models/lora/miles"
 accelerate launch --mixed_precision="fp16"  ../src/train_text_to_image_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --train_data_dir=$DATA_DIR \
+  --caption_column="text" \
+  --resolution=512 --random_flip \
+  --train_batch_size=1 \
+  --num_train_epochs=100 \
   --dataloader_num_workers=8 \
   --resolution=512 --center_crop --random_flip \
-  --train_batch_size=1 \
   --gradient_accumulation_steps=4 \
   --max_train_steps=1500 \
   --learning_rate=1e-04 \
   --max_grad_norm=1 \
-  --lr_scheduler="cosine" --lr_warmup_steps=0 \
+  --lr_scheduler="constant" --lr_warmup_steps=0 \
   --output_dir=${OUTPUT_DIR} \
-  --checkpointing_steps=500 \
+  --checkpointing_steps=5000 \
   --validation_prompt="A photo of a cat in a bucket" \
   --validation_epochs=10 \
   --seed=42
