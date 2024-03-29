9446b9b564672d6efef8d6be4188585688dfc87e
sejoung kim <sejoung@gmail.com>
2024-03-29 18:08:41 +0900

    드림부스

--- bin/cat_mailes_train_dreambooth_lora.sh
+++ bin/cat_mailes_train_dreambooth_lora.sh
@@ -24,7 +24,7 @@ accelerate launch ../src/train_dreambooth_lora.py \
   --train_batch_size=2 \
   --gradient_accumulation_steps=1 \
   --checkpointing_steps=100 \
-  --learning_rate=1e-4 \
+  --learning_rate=1e-04 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --max_train_steps=1500 \
