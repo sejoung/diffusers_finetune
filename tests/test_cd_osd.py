263797cd7910e1d751485bfcd8af7e62ca6551fa
sejoung kim <sejoung@gmail.com>
2024-04-19 11:40:52 +0900

    링크

--- tests/test_cd_osd.py
+++ tests/test_cd_osd.py
@@ -7,7 +7,7 @@ from convert_diffusers_to_original_stable_diffusion import get_state_dict
 def test_convert_to_pathlib():
   pipeline = DiffusionPipeline.from_pretrained(
     "stablediffusionapi/anything-v5",
-    torch_dtype="fp32",
+    torch_dtype="auto",
   )
   output = "/Users/beni/Downloads/diffusers_model_original"
   pipeline.save_pretrained(output)
