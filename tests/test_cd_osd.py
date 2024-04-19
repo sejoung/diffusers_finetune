bfa9a9f35cece2ae2a18f78ee7091867256a8a3d
sejoung kim <sejoung@gmail.com>
2024-04-19 11:37:14 +0900

    링크

--- tests/test_cd_osd.py
+++ tests/test_cd_osd.py
@@ -1,8 +1,15 @@
+from diffusers import DiffusionPipeline
 from safetensors.torch import save_file
 
 from convert_diffusers_to_original_stable_diffusion import get_state_dict
 
 
 def test_convert_to_pathlib():
-  state_dict = get_state_dict("/Users/beni/anything-v5/")
+  pipeline = DiffusionPipeline.from_pretrained(
+    "stablediffusionapi/anything-v5",
+    torch_dtype="fp32",
+  )
+  output = "/Users/beni/Downloads/diffusers_model_original"
+  pipeline.save_pretrained(output)
+  state_dict = get_state_dict(output)
   save_file(state_dict, "/Users/beni/Downloads/diffusers_model_original.safetensors")
